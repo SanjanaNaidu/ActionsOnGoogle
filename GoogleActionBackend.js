c65aa44a0524965ecf4a2db42deaa011575fa7d6
V R KARTHIK <vrkarthik14@gmail.com>
2019-02-16 09:41:54 +0530

    updated backend

--- GoogleActionBackend.js
+++ GoogleActionBackend.js
@@ -1,16 +1,3 @@
-// Copyright 2018, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 "use strict";
 
 const {
@@ -20,23 +7,36 @@ const {
   Suggestions
 } = require("actions-on-google");
 
+const admin = require("firebase-admin");
+
+//Calling sms-one lambda in this lamda funtion
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+var lambda = new AWS.Lambda();
+
 // Instantiate the Dialogflow client.
 const app = dialogflow({ debug: true });
 
+var serviceAccount = require("./acc.json");
+var firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://actionson-4001e.firebaseio.com"
+  });
+
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent("Default Welcome Intent", conv => {
-  conv.user.storage = {};
+  // conv.user.storage = {};
   const name = conv.user.storage.userName;
   if (!name) {
     // Asks the user's permission to know their name, for personalization.
     conv.ask(
       new Permission({
-        context: "Hi there, to get to know you better",
+        context: "Hi there,Let's get connected,Register with us. To get to know you better",
         permissions: ['NAME', 'DEVICE_PRECISE_LOCATION']
       })
     );
   } else {
-    conv.ask(`Hi again, ${name}. What's your favorite color?`);
+    conv.ask(`Hi, ${name}. How can I assist you?`);
   }
 });
 
@@ -45,25 +45,71 @@ app.intent("Default Welcome Intent", conv => {
 app.intent("actions_intent_PERMISSION", (conv, params, permissionGranted) => {
   if (!permissionGranted) {
     // If the user denied our request, go ahead with the conversation.
-    conv.ask(`OK, no worries. What's your favorite color?`);
-    conv.ask(new Suggestions("Blue", "Red", "Green"));
+    conv.ask(`OK, no worries. What is you name?`);
+    // conv.ask(new Suggestions("Blue", "Red", "Green"));
   } else {
     // If the user accepted our request, store their name in
     // the 'conv.user.storage' object for the duration of the conversation.
     conv.user.storage.userName = conv.user.name.display;
     var location = conv.device.location.coordinates;
-    // Call DynamoDB to add the item to the table
+    conv.user.storage.location = location;
     conv.ask(
-      `Thanks, ${conv.user.storage.userName}. You are currnetly at ${location.latitude} ${location.longitude}`
+      `Thanks, ${conv.user.storage.userName}. Provide emergency contact number.`
     );
-    console.log("location is: "+location)
+    console.log("location is: "+location);
   }
 });
 
 app.intent("ContactNameIntent",(conv,{number}) => {
-  conv.ask(`Thanks for providing number.`);
+  return new Promise( function( resolve, reject ){
+    //retain number in persistance storage
+    conv.user.storage.contact_number = number;
+    var data = {
+    emergency_contact:{
+      num1:number
+    },
+    is_emergency:false
+  };
+  
+  firebase.database().ref("users").child(conv.user.storage.userName).set(data)
+  .then(()=>{
+      console.log("sucess");
+        firebase.delete();
+        conv.ask("We are connected. How can I assist you?")
+        resolve();
+    }).catch(function (error) {
+          console.log('Firebase error: ', error);
+          firebase.delete();
+          conv.ask("Sorry for inconvinience. There is some network issue. Please try again.")
+          reject();
+    });
+  })
 });
 
+app.intent("SendSOSIntent",conv => {
+ return new Promise(function(resolve,reject){
+    var map_query = "https://maps.google.com/?q="+conv.user.storage.location.latitude+","+conv.user.storage.location.longitude;
+    //var num = "+91"+conv.user.storage.contact_number;
+    var params = {
+    FunctionName: 'sms-one', // the lambda function we are going to invoke
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail',
+    Payload: `{ "message" : "${map_query}"}`
+  };
+  lambda.invoke(params, function(err, data) {
+    if (err) {
+      // context.fail(err);
+      console.log("error occured",err);
+      reject();
+    } else {
+      // context.succeed('Lambda_B said '+ data.Payload);
+      console.log("Success");
+      conv.ask("Your request has be processed. Help is on the way.");
+      resolve();
+    }
+  })
+ })
+})
 
 app.intent("actions_intent_NO_INPUT", conv => {
   // Use the number of reprompts to vary response
@@ -78,6 +124,8 @@ app.intent("actions_intent_NO_INPUT", conv => {
     );
   }
 });
+
+
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = app;
